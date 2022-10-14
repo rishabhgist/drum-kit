c3695987b974f81bb51753f8bb9885ef2fe2e596
Rishabh Gist <damyrachit@gmail.com>
2022-10-14 17:18:45 +0530

    added eventlistner to get key

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 let noOfDrums = document.querySelectorAll(".drum").length;
 for (let i = 0; i < noOfDrums; i++){
     document.querySelectorAll(".drum")[i].addEventListener("click", function () {
-        let button = this.innerHTML;
-        switch (button) {
+        makeSound(this.innerHTML)
+    });
+}
+
+document.addEventListener("keypress", function () {
+    makeSound(event.key)
+})
+
+function makeSound(key) {
+            switch (key) {
             case "w":
                 let tom1 = new Audio("sounds/tom-1.mp3");
                 tom1.play();
@@ -35,5 +43,4 @@ for (let i = 0; i < noOfDrums; i++){
                 console.log();
                 break;
         }
-    });
 }
